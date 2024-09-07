ad7ac50cc3a01076441c540c24e820d6bcf32b25
GonzaSegu <gonzalo.seguel@gsti.cl>
2024-09-07 12:44:55 -0400

    agregada imagen landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import {Link} from 'react-router-dom'
+import image from '../assets/imagen.jpg'
 
 export default function Landing() {
   return (
         <div className='landingPage'>
-            <h1>Bienvenidos a Rick and Morty</h1>
-            <Link to='/home'><button className='btn btn-primary'>Entrar</button></Link>    
+          <img style={{width:"30rem", borderRadius:"20px"}} src={image} alt='Hero imagen'/>
+          <h1>Bienvenidos a Rick and Morty</h1>
+          <Link to='/home'><button className='btn btn-primary'>Entrar</button></Link>    
         </div>
   )
 }
